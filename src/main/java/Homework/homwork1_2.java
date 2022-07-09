af0e6b5619b2bef1dd301adb0cab66c58e681e67
GitJG <cemileqarayeva93@gmail.com>
2022-07-09 20:44:29 +0400

    added: tasks

--- src/main/java/Homework/homwork1_2.java
+++ src/main/java/Homework/homwork1_2.java
@@ -1,19 +1,17 @@
 package Homework;
-
-import java.io.BufferedReader;
 import java.util.Random;
-import java.util.Scanner;
 
 public class homwork1_2 {
         public static void main(String[] args) {
                 String[][] arr = {{"1914","1939","1988","2020"},
-                        {"World War I", "World War II","First Nagorno-Karabakh War","The Second Karabakh War"}};
-                for(int i=0;i<arr.length;i++){
-                        for(int j=0;j<arr[i].length;j++){
-                                System.out.print(arr[i][j]+" ");
-                        }
-                        System.out.println();
-                }
+                                        {"World War I", "World War II","First Nagorno-Karabakh War","The Second Karabakh War"}};
+//                for(int i=0;i<arr.length;i++){
+//                        for(int j=0;j<arr[i].length;j++){
+//                                System.out.print(arr[i][j]+" ");
+//                        }
+//                        System.out.println();
+//                }
+                System.out.println();
                 int rnd = new Random().nextInt(3);
                 System.out.println("When did the "+ arr[1][rnd] +" begin?" + " - " + arr[0][rnd]);
         }
