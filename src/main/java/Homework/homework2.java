da95679c3f51b77044dc6d049ac64d5e4e45efd7
GitJG <cemileqarayeva93@gmail.com>
2022-07-14 00:23:41 +0400

    added: tasks

--- src/main/java/Homework/homework2.java
+++ src/main/java/Homework/homework2.java
@@ -3,12 +3,13 @@ import java.util.Random;
 import java.util.Scanner;
 public class homework2 {
     public static void main(String[] args) {
-        System.out.println("Get ready to rumble!");
         Scanner sc = new Scanner(System.in);
-        String[][] arr = new String[5][5];
-        int[][] arr_pos = new int[5][5]; //positions
-
         Random rnd = new Random();
+        String[][] arr = new String[5][5];
+        int[][] arr_pos = new int[25][2];// positions
+        int arr_ind = 0;
+
+        System.out.println("Get ready to rumble!");
         for (int i = 0; i < 5; i++) {
             for (int j = 0; j < 5; j++) {
                 arr[i][j] = " - ";
@@ -16,11 +17,14 @@ public class homework2 {
             }
             System.out.println();
         }
+
         System.out.println("----------");
+
         int r_row = rnd.nextInt(5);
         int r_col = rnd.nextInt(5);
         System.out.println("rnd " + r_row+ " " + r_col);
-        while(true) {
+
+        Lb: while(true) {
             System.out.print("ENTER Row id: ");
             String s_row = sc.nextLine();
             System.out.print("ENTER Col id: ");
@@ -29,25 +33,30 @@ public class homework2 {
                 int row = Integer.parseInt(s_row);
                 int col = Integer.parseInt(s_col);
                 if (row < 5 && col < 5) {
+                    for (int a = 0; a < arr_ind; a++) {
+                        if (arr_pos[a][0] == row && arr_pos[a][1] == col) {
+                            System.out.println("this id entered previously please Enter again new id:");
+                            continue Lb;
+                        }
+                    }
                     if (row == r_row && col == r_col) {
                         System.out.println("You have won!!!");
                         for (int i = 0; i < 5; i++) {
                             for (int j = 0; j < 5; j++) {
                                 arr[row][col] = " X ";
-                                arr_pos[row][col] = Integer.parseInt(String.valueOf(r_row) + String.valueOf(r_col));
                                 System.out.print(arr[i][j]);
                             }
                             System.out.println();
                         }
                         break;
                     } else {
-                        int ind = Integer.parseInt(String.valueOf(row) + String.valueOf(col));
                         for (int i = 0; i < 5; i++) {
                             for (int j = 0; j < 5; j++) {
                                 if (i == row && j == col) {
                                     arr[row][col] = " * ";
-                                    arr_pos[row][col] = ind;//  position[]
+                                    arr_pos[arr_ind] = new int[]{row,col};
                                     System.out.print(arr[i][j]);
+                                    arr_ind++;
                                 } else {
                                     System.out.print(arr[i][j]);
                                 }
@@ -59,18 +68,8 @@ public class homework2 {
                     System.out.println("Pls ENTER Row & col id again in the range of playing field lines (1-5)");
                 }
             } catch (Exception e) {
-                System.out.print("Try again: ");
+                System.out.println("Try again: ");
             }
         }
-
-
-//                for ( int i = 0; i<5;i++){
-//                        for (int j = 0; j<5 ; j++){
-//
-//                                System.out.print(arr_pos[i][j]);
-//                        }
-//                        System.out.println();
-//                }
     }
-    }
-
+}
